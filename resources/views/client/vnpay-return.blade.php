5a6c0a7ba7a67cc6fbb8a38cec99985913151e71
unknown <thittth1807067@fpt.edu.vn>
2019-09-26 03:41:39 +0700

    trang vnpay

--- resources/views/client/vnpay-return.blade.php
+++ resources/views/client/vnpay-return.blade.php
@@ -1,48 +1,63 @@
 @extends('client.layout')
 
 @section('content')
-    <div class="container">
+    <div class="container vn-pay-body ">
+    <div class="vn-pay">
         <div class="header clearfix">
-            <h3 class="text-muted">VNPAY RESPONSE</h3>
+            <h3 class="text-danger text-center pt-sm-4 pb-sm-4">VNPAY RESPONSE</h3>
         </div>
         <div class="table-responsive">
             <div class="form-group">
-                <label >Mã đơn hàng:</label>
-                <label><?php echo $_GET['vnp_TxnRef'] ?></label>
-            </div>
-            Name {{$order->ship_name}}
-            Phone {{$order->ship_phone}}
-            Địa chỉ {{$order->ship_address}}
-            <div class="form-group">
-                <label >Số tiền:</label>
-                <label><?php echo number_format($_GET['vnp_Amount']) ?> VNĐ</label>
+                <div class="row vn-pay-row">
+                <span class="col-sm-12"><pre><b>Mã đơn hàng</b>: <?php echo $_GET['vnp_TxnRef'] ?></pre></span>
+
+                </div>
             </div>
             <div class="form-group">
-                <label >Nội dung thanh toán:</label>
-                <label><?php echo $_GET['vnp_OrderInfo'] ?></label>
+                <div class="row vn-pay-row">
+                    <span class="col-sm-6">  <pre><b>Name</b>: {{$order->ship_name}}</pre></span>
+                    <span class="col-sm-6">  <pre><b>Phone</b>: {{$order->ship_phone}}</pre></span>
+                </div>
             </div>
             <div class="form-group">
-                <label >Mã GD Tại VNPAY:</label>
-                <label><?php echo $_GET['vnp_TransactionNo'] ?></label>
+                <div class="row vn-pay-row">
+                    <span class="col-sm-12"><pre><b>Địa chỉ</b>: {{$order->ship_address}}</pre></span>
+                </div>
             </div>
             <div class="form-group">
-                <label >Mã Ngân hàng:</label>
-                <label><?php echo $_GET['vnp_BankCode'] ?></label>
+                <div class="row vn-pay-row">
+                   <span class="col-sm-5"><pre><b>Số tiền</b>: <?php echo number_format($_GET['vnp_Amount']) ?> VNĐ.</pre></span>
+                    <span class="col-sm-7"><pre><b>Thời gian thanh toán</b>: <?php echo $_GET['vnp_PayDate'] ?></pre></span>
+                </div>
             </div>
             <div class="form-group">
-                <label >Thời gian thanh toán:</label>
-                <label><?php echo $_GET['vnp_PayDate'] ?></label>
+                <div class="row vn-pay-row">
+                    <span class="col-sm-12"><pre><b>Nội dung thanh toán</b>: <?php echo $_GET['vnp_OrderInfo'] ?></pre></span>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="row vn-pay-row">
+                    <span class="col-sm-6"><pre><b>Mã GD Tại VNPAY</b>: <?php echo $_GET['vnp_TransactionNo'] ?></pre></span>
+                    <span class="col-sm-6"><pre><b>Mã Ngân hàng</b>: <?php echo $_GET['vnp_BankCode'] ?></pre></span>
+                </div>
             </div>
             <div class="form-group">
-                <label >Kết quả:</label>
+                <div class="row vn-pay-row">
+                <pre>  <b>Kết quả:</b> </pre>
                 @if($secureHash == $_GET['vnp_SecureHash'])
                     @if($_GET['vnp_ResponseCode'] == '00')
-                        <label>Giao dịch thành công</label>
+                        <label>Giao dịch thành công!</label>
                     @else
-                        <label>Giao dịch không thành công</label>
+                        <label>Giao dịch không thành công!</label>
                     @endif
                 @endif
+                </div>
             </div>
         </div>
+    </div>
+    <div class="text-right vn-pay-bt" >
+        <a href="/cart" class=" btn btn-outline-dark">Quay lại giỏ hàng </a>
+    </div>
     </div>
 @endsection
